f3a5322d224ff47ee34b3cd6ffaa3229d8560f71
Damien Villeneuve <BlinkSun@users.noreply.github.com>
2014-06-19 12:56:43 -0400

    Update mytest.php

--- mytest.php
+++ mytest.php
@@ -1,16 +1,46 @@
-<?php
-$img = imagecreatefromjpeg("http://www.publicdomainpictures.net/pictures/60000/nahled/checkerboard-squares-black-white.jpg");
-$pixel_x = imagesx($img);
-$pixel_y = imagesy($img); 
+Const MAZE_X = 15;
+Const MAZE_Y = 15;
 
-$pixel_rgb = imagecolorat($img, 90, 30);
-$r = ($pixel_rgb >> 16) & 0xFF;
-$g = ($pixel_rgb >> 8) & 0xFF;
-$b = $pixel_rgb & 0xFF;
+$cDir = array_fill(0, 4, array('X' => NULL, 'Y' => NULL));
+$blnMaze = array_fill(0, MAZE_X, array_fill(0, MAZE_Y, NULL));
 
-var_dump($r, $g, $b);
-echo $pixel_rgb . " ";
-echo $pixel_x . "x" . $pixel_y;
+function RandomDirections($cDir) {
+	
+        switch (rand(0,3)) {
+		Case 0:
+			$cDir[0]['X'] = -1;
+			$cDir[1]['X'] = 1;
+			$cDir[2]['Y'] = -1;
+			$cDir[3]['Y'] = 1;
+		break;
+		Case 1:
+			$cDir[3]['X'] = -1;
+			$cDir[2]['X'] = 1;
+			$cDir[1]['Y'] = -1;
+			$cDir[0]['Y'] = 1;
+		break;
+		Case 2:
+			$cDir[2]['X'] = -1;
+			$cDir[3]['X'] = 1;
+			$cDir[0]['Y'] = -1;
+			$cDir[1]['Y'] = 1;
+		break;
+		Case 3:
+			$cDir[1]['X'] = -1;
+			$cDir[0]['X'] = 1;
+			$cDir[3]['Y'] = -1;
+			$cDir[2]['Y'] = 1;
+		break;
+	}
+        return $cDir;
+}
 
+function IsFree($cF) {
+    if($cF['X'] < MAZE_X and $cF['X'] > 1 and $cF['Y'] < MAZE_Y and $cF['Y'] > 1) {
+        return $blnMaze[$cF['X']][$cF['Y']] = 0;
+    }
+}
 
-?> 
+$cDir = array_fill(0, 4, array('X' => NULL, 'Y' => NULL));
+$cDir = RandomDirections($cDir);
+var_dump($cDir);
