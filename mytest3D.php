7cd2a5827771047eeddabcb596d50ea05ac6db14
Damien Villeneuve <BlinkSun@users.noreply.github.com>
2014-06-26 02:52:14 -0400

    Update mytest3D.php

--- mytest3D.php
+++ mytest3D.php
@@ -1,7 +1,7 @@
 <?PHP
 function RandomDirections() {
 	$cDir = array_fill(0, 6, array('X' => 0, 'Y' => 0, 'Z' => 0));
-	switch (rand(0,1)) {
+	switch (rand(0,5)) {
 		Case 0:
 		$cDir[0]['X'] = -1;
 		$cDir[1]['X'] = 1;
@@ -18,18 +18,38 @@ function RandomDirections() {
 		$cDir[1]['Z'] = -1;
 		$cDir[0]['Z'] = 1;
 		break;
-/*		Case 2:
+		Case 2:
 		$cDir[2]['X'] = -1;
 		$cDir[3]['X'] = 1;
+		$cDir[4]['Y'] = -1;
+		$cDir[5]['Y'] = 1;
 		$cDir[0]['Z'] = -1;
 		$cDir[1]['Z'] = 1;
 		break;
 		Case 3:
 		$cDir[1]['X'] = -1;
 		$cDir[0]['X'] = 1;
+		$cDir[5]['Y'] = -1;
+		$cDir[4]['Y'] = 1;
 		$cDir[3]['Z'] = -1;
 		$cDir[2]['Z'] = 1;
-		break; */
+		break; 
+		Case 4:
+		$cDir[4]['X'] = -1;
+		$cDir[5]['X'] = 1;
+		$cDir[0]['Y'] = -1;
+		$cDir[1]['Y'] = 1;
+		$cDir[2]['Z'] = -1;
+		$cDir[3]['Z'] = 1;
+		break;
+		Case 5:
+		$cDir[3]['X'] = -1;
+		$cDir[2]['X'] = 1;
+		$cDir[1]['Y'] = -1;
+		$cDir[0]['Y'] = 1;
+		$cDir[5]['Z'] = -1;
+		$cDir[4]['Z'] = 1;
+		break;
 	}
 	return $cDir;
 }
@@ -46,13 +66,37 @@ function GenerateMaze($MAZE_X, $MAZE_Y, $MAZE_Z) {
 	$cDir = array();
 	$blnMaze = array_fill(1, $MAZE_X, array_fill(1, $MAZE_Y, array_fill(1, $MAZE_Z, False)));
 
+	
+/*for($x = 1; $x <= $MAZE_X; $x++)
+{
+    for($y = 1; $y <= $MAZE_Y; $y++)
+    {
+        for($z = 1; $z <= $MAZE_Z; $z++)
+        {
+            $blnMaze[$x][$y][$z] = False;
+        }
+    }
+}*/
+	
+	
+	//echo "Let start it !<br>";
 
 	Do {
+		//echo "Loop #" . $intDone . "<br>";
+		
 		$cS['X'] = round(rand(1, $MAZE_X - 1) / 2) * 2;
 		$cS['Y'] = round(rand(1, $MAZE_Y - 1) / 2) * 2;
 		$cS['Z'] = round(rand(1, $MAZE_Z - 1) / 2) * 2;
+		
+		//echo "checking blnMaze(" . $cS['X'] . "," . $cS['Y'] . "," . $cS['Z'] . ")...(".$blnMaze[$cS['X']][$cS['Y']][$cS['Z']].")<br>";
+		
 		if($intDone == 0) {
+		
+			$cS['X'] = 2;
+			$cS['Y'] = 2;
+			$cS['Z'] = 2;
 			$blnMaze[$cS['X']][$cS['Y']][$cS['Z']] = True;
+			
 		}
 		if($blnMaze[$cS['X']][$cS['Y']][$cS['Z']] == True) {
 			Do {
@@ -63,7 +107,11 @@ function GenerateMaze($MAZE_X, $MAZE_Y, $MAZE_Z) {
 					$cN['Y'] = $cS['Y'] + ($cDir[$intDir]['Y'] * 2);
 					$cN['Z'] = $cS['Z'] + ($cDir[$intDir]['Z'] * 2);
 					if($cN['X'] < $MAZE_X and $cN['X'] > 1 and $cN['Y'] < $MAZE_Y and $cN['Y'] > 1 and $cN['Z'] < $MAZE_Z and $cN['Z'] > 1) {
-						if($blnMaze[$cS['X']][$cS['Y']][$cS['Z']] == False) {
+						//echo "blnMaze(" . $cN['X'] . "," . $cN['Y'] . "," . $cN['Z'] . ") = ".$blnMaze[$cN['X']][$cN['Y']][$cN['Z']]."<br>";
+						if($blnMaze[$cN['X']][$cN['Y']][$cN['Z']] == False) {
+						
+							//echo "blnMaze(" . $cN['X'] . "," . $cN['Y'] . "," . $cN['Z'] . ") = True<br>";
+						
 							$blnMaze[$cS['X']][$cS['Y']][$cS['Z']] = True;
 							$blnMaze[$cS['X'] + $cDir[$intDir]['X']][$cS['Y'] + $cDir[$intDir]['Y']][$cS['Z'] + $cDir[$intDir]['Z']] = True;
 							$cS['X'] = $cN['X'];
@@ -78,16 +126,28 @@ function GenerateMaze($MAZE_X, $MAZE_Y, $MAZE_Z) {
 			} while ($blnBlocked == False);
 		}
 	} while ($intDone + 1 < (($MAZE_X - 1) * ($MAZE_Y - 1) * ($MAZE_Z - 1)) / 8);
-	
+
 	//$blnMaze[1][2] = True;					//Start hole
 	//$blnMaze[$MAZE_X][$MAZE_Z - 1] = True;	//Exit hole
+
+	//echo "<br>".($intDone + 1). " / " . (($MAZE_X - 1) * ($MAZE_Y - 1) * ($MAZE_Z - 1)) / 8 . "<br><br>";
+	
+echoMaze($MAZE_X,$MAZE_Y,$MAZE_Z,$blnMaze);
+}
+function echoMaze($MAZE_X,$MAZE_Y,$MAZE_Z,$blnMaze) {
 
 	For($y = 1; $y <= $MAZE_Y; $y++) {
 		$output = "Level ".$y.":<br><pre>";
 		For($z = 1; $z <= $MAZE_Z; $z++) {
 			For($x = 1; $x <= $MAZE_X; $x++) {
 				If($blnMaze[$x][$y][$z] == True) {
-					$output = $output . "&nbsp;";
+				
+					if($blnMaze[$x][$y + 1][$z] == True or $blnMaze[$x][$y - 1][$z] == True) {
+						$output = $output . "S";
+					} else {
+						$output = $output . "&nbsp;";
+					}
+					
 				} Else {
 					$output = $output . "#";
 				}
@@ -96,7 +156,7 @@ function GenerateMaze($MAZE_X, $MAZE_Y, $MAZE_Z) {
 		}
 		echo $output . "</pre><br>";
 	}
-}
 
-GenerateMaze(5, 5, 3);
+}
+GenerateMaze(25, 7, 15);
 ?>
